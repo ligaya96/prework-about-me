ec775d1b4dc5934d66905462f0a5c30a8d63a97b
Ligaya <ligaya96@gmail.com>
2020-12-11 13:24:48 -0800

    add other files from prework

---
+++ javascript.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+    document.getElementById("growBtn").addEventListener("click", function(){
+        
+        document.getElementById("box").style.height = "250px";
+    }):
+    document.getElementById("button2").addEventListener("click", function(){
+        document.getElementById("box").style.background-color ="blue";
+      
+    }):
+    
